6d7162d14263f99b2da2c93fa7ff6146d5f15e22
manso13 <lcarlosmanso@hotmail.com>
2013-11-01 16:19:19 +0000

    Inicio implementação menu feed

---
+++ Spikes/IplSA/src/com/example/iplsa/MenuFeedActivity.java
@@ -0,0 +1,58 @@
+package com.example.iplsa;
+
+import java.util.ArrayList;
+
+import com.example.iplsa.Classes.RowItem;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MenuFeedActivity extends Activity {
+
+	private Runnable getInfoFromWebservices;
+	
+	private ArrayList<RowItem> feedRows = new ArrayList<RowItem>();
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menu_feed);
+		
+		//Chamar thread para consumir webservices
+		
+		fillFeed();
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_feed, menu);
+		return true;
+	}
+	
+	public void configButtonPressed(View v)
+	{
+//		EditText username= (EditText)findViewById(R.id.editText1);
+//		String user = username.getText().toString();
+//		
+//		EditText password= (EditText)findViewById(R.id.editText2);
+//		String pass = password.getText().toString();
+//		
+		Intent intent = new Intent(this,ConfigOptionsActivity.class);
+		startActivity(intent);	
+	}
+	
+	public boolean fillFeed()
+	{
+				
+		//TODO
+		
+		return true;
+	}
+
+}
